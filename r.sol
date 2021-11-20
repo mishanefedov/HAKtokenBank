fd482b8205de3fd90b165615dedd7b2360d0dfa9
Michael Nefedov <michael.nefedov@tum.de>
2021-11-20 13:02:56 +0100

    m

--- r.sol
+++ r.sol
@@ -4,6 +4,10 @@ import './bank.sol';
 import './IPriceOracle.sol';
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
+
+contract Account {
+    
+}
 contract Bank is IBank {
 
     mapping (address => uint) depositsETH;
@@ -76,19 +80,22 @@ contract Bank is IBank {
 
 
     function liquidate(address token, address account) onlyOwner payable external override returns (bool) {
+        require(getCollateralRatio(token, account) <15000);
         IERC20 tokenID = token;
-        if (getCollateralRatio(tokenID,account)<15000) {
-            
-            uint amountOnDeposit = deposits[account] +accruedInterest[account];
-            uint amountBorrowed = borrowed[account]+owedInterest[account];
-            deposits[account]=0;
-            accruedInterest[account]= 0;
-            borrowed[account]=0;
-            owedInterest[account]=0;
-            depositsETH[this] -= amountBorrowed;
-            depositsHAK[this] +=amountOnDeposit;
-            return true;
-        } else revert();
+        if (tokenID == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) { //deposit ETH
+            return depositETH[msg.sender] * 1.03;
+        } else if (tokenID == 0xBefeeD4CB8c6DD190793b1c97B72B60272f3EA6C) { //deposit HAK
+            return depositHAK[msg.sender] * 1.03;
+        }
+        uint256 amountOnDeposit = 
+        uint256 amountBorrowed = borrowed[account]+owedInterest[account];
+        deposits[account]=0;
+        accruedInterest[account]= 0;
+        borrowed[account]=0;
+        owedInterest[account]=0;
+        depositsETH[this] -= amountBorrowed;
+        depositsHAK[this] +=amountOnDeposit;
+        return true;
     }
 
 
@@ -97,8 +104,7 @@ contract Bank is IBank {
     function getCollateralRatio(address token, address account) onlyOwner view external override returns (uint256) {
         IERC20 tokenID = token;
         PriceOracle po = PriceOracle();
-        if (tokenId = 0xBefeeD4CB8c6DD190793b1c97B72B60272f3EA6C)
-        {
+        if (tokenId = 0xBefeeD4CB8c6DD190793b1c97B72B60272f3EA6C){
             return po.call.getVirtualPrice(tokenID)*depositsHAK[account]*10000/ borrowedETH[account];
         } else revert();
     }
